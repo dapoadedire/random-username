7135b2a644a88919b22f3777e1124a0db9f2d3d6
dapoadedire <adedireadedapo19@gmail.com>
2022-05-22 08:23:33 +0100

    Initial commit

---
+++ randomusername/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RandomusernameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'randomusername'
---
+++ randomusername/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from faker import Faker
+import random
+# Create your views here.
+
+def index(request):
+    fake = Faker()
+    initials = fake.name()
+    n = random.randint(3, 6)
+    m = random.randint(4, 6)
+    initials = initials.split()[0][0:n]  + initials.split()[1][0:m]
+    num = random.randint(1,99)
+    name = initials.lower() + str(num)
+    return render(request, 'index.html', {'name': name})
